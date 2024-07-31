46a65dc08d7f00e9e941f6f063a72ffe60d8a95e
korawat lawanwong <korawatlawan@gmail.com>
2024-07-31 00:05:43 -0700

    first commit

--- src/app/pages/index/index.page.spec.ts
+++ src/app/pages/index/index.page.spec.ts
@@ -1,15 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IndexPage } from './index.page';
 
-import { HomePage } from './home.page';
+describe('IndexPage', () => {
+  let component: IndexPage;
+  let fixture: ComponentFixture<IndexPage>;
 
-
-
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
-
-  beforeEach(async () => {
-    fixture = TestBed.createComponent(HomePage);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(IndexPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
